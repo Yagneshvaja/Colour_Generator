9f7937d09abfa4794b7ee408ee3249c59411da69
Yagnesh <109731363+Yagneshvaja@users.noreply.github.com>
2023-09-23 12:05:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const getcolor = () =>{
+
+    const randomNumber=Math.floor(Math.random() * 16777215);
+    const randomCode="#"+randomNumber.toString(16);
+    document.body.style.backgroundColor= randomCode;
+    document.getElementById("colour").innerText =randomCode;
+
+    navigator.clipboard.writeText(randomCode)
+
+    document.getElementById("btn").addEventListener(
+        "click",
+        getcolor
+    )
+
+}
+
+getcolor();
+
